6515d980e536d3957a0e758f6f82a258eec3908f
Ирина Капкова <kapiral@rambler.ru>
2022-12-11 22:27:33 +0400

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" ");
+
+        boolean success =Data.validate("123456q_1231231231322222222222222222222222222222222222", "123456q_", "123456q_");
+        if (success){
+            System.out.println("Данные корректные");
+        } else {
+            System.out.println("Введенные логин и/или пароль не прошли проверку");
+        }
+
     }
 }
